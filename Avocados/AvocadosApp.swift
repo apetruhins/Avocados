b8fdf2708964d188aeb7bd6dcde21f7bda619bfe
Aleksandrs Petruhins <aleksandrs.petruhins@gmail.com>
2022-07-12 23:13:58 +0300

    Initial commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Alex on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Avocados
+Avocados app from Udemy course SwiftUI Masterclass 2022 - iOS App Development &amp; Swift
